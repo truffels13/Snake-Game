61ce80b540ab597a292181d45b85e0e6d41e545b
Ben <b.ralton@icloud.com>
2017-05-21 17:44:03 +0100

    Ran autoformatting on file.
    Changed if bocks to if-elif blocks.
    
    Refactored game over conditions into their own function.

--- Python/snakegame.py
+++ Python/snakegame.py
@@ -7,10 +7,10 @@ import pygame, sys, random, time
 check_errors = pygame.init()
 
 if check_errors[1] > 0:
-    print ("(!) Had {0} initialising errors, exiting...".format(check_errors[1]))
+    print("(!) Had {0} initialising errors, exiting...".format(check_errors[1]))
     sys.exit(-1)
 else:
-    print ("(+) Success")
+    print("(+) Success")
 
 # Play Surface
 playSurface = pygame.display.set_mode((720, 460))
@@ -36,6 +36,7 @@ changeTo = direction
 
 score = 0
 
+
 def gameOver():
     myFont = pygame.font.SysFont('monaco', 72)
     gameOverSurface = myFont.render('Game Over!', True, red)
@@ -45,10 +46,11 @@ def gameOver():
     showScore(2)
     pygame.display.flip()
     time.sleep(4)
-    pygame.quit() #pygame exit
-    sys.exit() #console exit
+    pygame.quit()  # pygame exit
+    sys.exit()  # console exit
 
-def showScore(choice = 1):
+
+def showScore(choice=1):
     myFont = pygame.font.SysFont('monaco', 24)
     scoreSurface = myFont.render('Score : {0}'.format(score), True, black)
     scoreRectangle = scoreSurface.get_rect()
@@ -58,6 +60,18 @@ def showScore(choice = 1):
         scoreRectangle.midtop = (360, 120)
     playSurface.blit(scoreSurface, scoreRectangle)
 
+
+def gameOverConditions():
+    if snakePos[0] > 710 or snakePos[0] < 0:
+        gameOver()
+    if snakePos[1] > 450 or snakePos[1] < 0:
+        gameOver()
+
+    for bodyBlock in snakeBody[1:]:
+        if bodyBlock[0] == snakePos[0] and bodyBlock[1] == snakePos[1]:
+            gameOver()
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -66,35 +80,35 @@ while True:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT or event.key == ord('d'):
                 changeTo = 'RIGHT'
-            if event.key == pygame.K_LEFT or event.key == ord('a'):
+            elif event.key == pygame.K_LEFT or event.key == ord('a'):
                 changeTo = 'LEFT'
-            if event.key == pygame.K_UP or event.key == ord('w'):
+            elif event.key == pygame.K_UP or event.key == ord('w'):
                 changeTo = 'UP'
-            if event.key == pygame.K_DOWN or event.key == ord('s'):
+            elif event.key == pygame.K_DOWN or event.key == ord('s'):
                 changeTo = 'DOWN'
-            if event.key == pygame.K_ESCAPE:
+            elif event.key == pygame.K_ESCAPE:
                 pygame.event.post(pygame.event.Event(pygame.QUIT))
 
     if changeTo == 'RIGHT' and not direction == 'LEFT':
         direction = 'RIGHT'
-    if changeTo == 'LEFT' and not direction == 'RIGHT':
+    elif changeTo == 'LEFT' and not direction == 'RIGHT':
         direction = 'LEFT'
-    if changeTo == 'UP' and not direction == 'DOWN':
+    elif changeTo == 'UP' and not direction == 'DOWN':
         direction = 'UP'
-    if changeTo == 'DOWN' and not direction == 'UP':
+    elif changeTo == 'DOWN' and not direction == 'UP':
         direction = 'DOWN'
 
     if direction == 'RIGHT':
         snakePos[0] += 10
-    if direction == 'LEFT':
+    elif direction == 'LEFT':
         snakePos[0] -= 10
-    if direction == 'UP':
+    elif direction == 'UP':
         snakePos[1] -= 10
-    if direction == 'DOWN':
+    elif direction == 'DOWN':
         snakePos[1] += 10
 
     snakeBody.insert(0, list(snakePos))
-    if snakePos[0] == foodPos [0] and snakePos[1] == foodPos[1]:
+    if snakePos[0] == foodPos[0] and snakePos[1] == foodPos[1]:
         foodSpawn = False
         score += 1
     else:
@@ -111,14 +125,7 @@ while True:
 
     pygame.draw.rect(playSurface, brown, pygame.Rect(foodPos[0], foodPos[1], 10, 10))
 
-    if snakePos[0] > 710 or snakePos[0] < 0:
-        gameOver()
-    if snakePos[1] > 450 or snakePos[1] < 0:
-        gameOver()
-
-    for bodyBlock in snakeBody[1:]:
-        if bodyBlock[0] == snakePos[0] and bodyBlock[1] == snakePos[1]:
-            gameOver()
+    gameOverConditions()
 
     showScore()
     pygame.display.flip()
